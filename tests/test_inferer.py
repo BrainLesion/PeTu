1200a6c0e786e857d80fa67ef0e65325d3cbccaa
Marcel Rosier <marcel.rosier@gmx.de>
2025-02-25 08:18:06 +0100

    Fix wrongly patched inferer tests

--- tests/test_inferer.py
+++ tests/test_inferer.py
@@ -17,15 +17,19 @@ def inferer(mock_model_handler, mock_data_handler):
     return Inferer(device="cpu")
 
 
-def test_device_configuration_gpu():
+@patch("petu.inferer.torch.cuda.is_available", return_value=True)
+@patch("petu.inferer.DataHandler")
+@patch("petu.inferer.ModelHandler")
+def test_device_configuration_gpu(
+    mock_model_handler, mock_data_handler, mock_cuda_is_available
+):
     inferer_gpu = Inferer(device="cuda", cuda_visible_devices="0")
-    if torch.cuda.is_available():
-        assert inferer_gpu.device.type == "cuda"
-    else:
-        assert inferer_gpu.device.type == "cpu"
+    assert inferer_gpu.device.type == "cuda"
 
 
-def test_device_configuration_cpu():
+@patch("petu.inferer.DataHandler")
+@patch("petu.inferer.ModelHandler")
+def test_device_configuration_cpu(mock_model_handler, mock_data_handler):
     inferer_cpu = Inferer(device="cpu")
     assert inferer_cpu.device.type == "cpu"
 
